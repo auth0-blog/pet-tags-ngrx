9ffdbe96009e5631996f271f4dd4e752e45261b8
Kim Maida <kim.maida@auth0.com>
2017-02-09 11:32:12 -0500

    Customize app component and add .vscode settings.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './core/app.component';
+import { TagBuilderComponent } from './tag-builder/tag-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagBuilderComponent
   ],
   imports: [
     BrowserModule,
