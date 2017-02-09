4ac103bbe7f522be0f185ffc5c7e0758d158975c
Kim Maida <kim.maida@auth0.com>
2017-02-09 11:12:18 -0500

    Revert prefix change (for simplification)

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pt-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
