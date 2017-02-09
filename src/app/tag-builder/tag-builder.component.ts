20ee76de1dbc0013e0fdea2a8c361488ca79851d
Kim Maida <kim.maida@auth0.com>
2017-02-09 11:32:25 -0500

    Add tag builder component scaffolding.

---
+++ src/app/tag-builder/tag-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-builder',
+  templateUrl: './tag-builder.component.html',
+  styleUrls: ['./tag-builder.component.css']
+})
+export class TagBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
